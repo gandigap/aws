1eb9c49e34c407aae70ae5144289b16975345486
iharberasneu <iberesnev@alfabank.ru>
2024-06-28 14:32:29 +0300

    refactor(*): update api

--- api/index.js
+++ api/index.js
@@ -1,70 +1,71 @@
+import express from "express";
+import serverless from "serverless-http";
+import cors from "cors";
+import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.js";
 
-import serverless from 'serverless-http'
-import express from 'express';
-import { fetchTasks, createTasks, updateTasks, deleteTasks } from './task'
-import cors from 'cors'
+const app = express();
+const port = 3001;
 
-const app = express()
-const port = 3001
+app.use(express.json());
 
-app.use(express.json())
-
-if(process.env.DEVELOPMENT){
+if (process.env.DEVELOPMENT) {
   app.use(cors());
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-app.get('/task', async (req, res) => {
+app.get("/task", async (req, res) => {
   try {
     const tasks = await fetchTasks();
-    res.send(tasks.item)
-  } catch (error) {
-    res.status(400).send(`Error fetching tasks: ${error}`)
-  }
-})
 
-app.post('/task', async (req, res) => {
+    res.send(tasks.Items);
+  } catch (err) {
+    res.status(400).send(`Error fetching tasks: ${err}`);
+  }
+});
+
+app.post("/task", async (req, res) => {
   try {
-    const tasks = req.body;
+    const task = req.body;
+
     const response = await createTasks(task);
 
     res.send(response);
-  } catch (error) {
-    res.status(400).send(`Error creating tasks: ${error}`)
+  } catch (err) {
+    res.status(400).send(`Error creating tasks: ${err}`);
   }
-})
+});
 
-app.put('/task', async (req, res) => {
+app.put("/task", async (req, res) => {
   try {
-    const tasks = req.body;
+    const task = req.body;
+
     const response = await updateTasks(task);
 
     res.send(response);
-  } catch (error) {
-    res.status(400).send(`Error updating tasks: ${error}`)
+  } catch (err) {
+    res.status(400).send(`Error updating tasks: ${err}`);
   }
-})
+});
 
-app.delete('/task/:id', async (req, res) => {
+app.delete("/task/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    
+
     const response = await deleteTasks(id);
 
     res.send(response);
-  } catch (error) {
-    res.status(400).send(`Error deleting tasks: ${error}`)
+  } catch (err) {
+    res.status(400).send(`Error deleting tasks: ${err}`);
   }
-})
+});
 
-if(process.env.DEVELOPMENT){
+if (process.env.DEVELOPMENT) {
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+    console.log(`Example app listening on port ${port}`);
+  });
 }
 
-export const handler = serverless(app)
+export const handler = serverless(app);
